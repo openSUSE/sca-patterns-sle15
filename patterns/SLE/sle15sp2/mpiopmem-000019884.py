5779b8dbc8f185ea6293caa89101f44077f8e0d3
Jason Record <jason.record@gmail.com>
2021-06-16 14:46:22 -0600

    Added bug number

--- patterns/SLE/sle15sp2/mpiopmem-000019884.py
+++ patterns/SLE/sle15sp2/mpiopmem-000019884.py
@@ -39,7 +39,7 @@ PATTERN_ID = os.path.basename(__file__)
 PRIMARY_LINK = "META_LINK_TID"
 OVERALL = Core.TEMP
 OVERALL_INFO = "NOT SET"
-OTHER_LINKS = "META_LINK_TID=https://www.suse.com/support/kb/doc/?id=000019884"
+OTHER_LINKS = "META_LINK_TID=https://www.suse.com/support/kb/doc/?id=000019884|META_LINK_BUG=https://bugzilla.suse.com/show_bug.cgi?id=1182917"
 
 Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
 
