f63266e383f9d65089e1de2fb27d5277edd6c101
Jason Record <jason.record@suse.com>
2022-05-06 09:46:15 -0600

    Jan 2022 New Security Announcement Patterns 92

---
+++ patterns/SLE/sle15sp1/libvirt_SUSE-SU-2022_0128-1_15.1.ltss.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+#
+# Title:       Important Security Announcement for libvirt SUSE-SU-2022:0128-1
+# Description: Security fixes for SUSE Linux Enterprise 15 SP1 LTSS
+# Source:      Security Announcement Parser v1.6.4
+# Modified:    2022 May 06
+#
+##############################################################################
+# Copyright (C) 2022 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record <jason.record@suse.com>
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "libvirt"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2022-January/010027.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'libvirt'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2022:0128-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 15):
+	if ( SERVER['DistroPatchLevel'] == 1 ):
+		PACKAGES = {
+			'libvirt': '5.1.0-17.1',
+			'libvirt-admin': '5.1.0-17.1',
+			'libvirt-admin-debuginfo': '5.1.0-17.1',
+			'libvirt-bash-completion': '5.1.0-17.1',
+			'libvirt-client': '5.1.0-17.1',
+			'libvirt-client-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon': '5.1.0-17.1',
+			'libvirt-daemon-config-network': '5.1.0-17.1',
+			'libvirt-daemon-config-nwfilter': '5.1.0-17.1',
+			'libvirt-daemon-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-interface': '5.1.0-17.1',
+			'libvirt-daemon-driver-interface-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-libxl': '5.1.0-17.1',
+			'libvirt-daemon-driver-libxl-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-lxc': '5.1.0-17.1',
+			'libvirt-daemon-driver-lxc-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-network': '5.1.0-17.1',
+			'libvirt-daemon-driver-network-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-nodedev': '5.1.0-17.1',
+			'libvirt-daemon-driver-nodedev-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-nwfilter': '5.1.0-17.1',
+			'libvirt-daemon-driver-nwfilter-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-qemu': '5.1.0-17.1',
+			'libvirt-daemon-driver-qemu-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-secret': '5.1.0-17.1',
+			'libvirt-daemon-driver-secret-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-core': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-core-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-disk': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-disk-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-iscsi': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-iscsi-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-logical': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-logical-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-mpath': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-mpath-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-rbd': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-rbd-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-scsi': '5.1.0-17.1',
+			'libvirt-daemon-driver-storage-scsi-debuginfo': '5.1.0-17.1',
+			'libvirt-daemon-hooks': '5.1.0-17.1',
+			'libvirt-daemon-lxc': '5.1.0-17.1',
+			'libvirt-daemon-qemu': '5.1.0-17.1',
+			'libvirt-daemon-xen': '5.1.0-17.1',
+			'libvirt-debugsource': '5.1.0-17.1',
+			'libvirt-devel': '5.1.0-17.1',
+			'libvirt-doc': '5.1.0-17.1',
+			'libvirt-libs': '5.1.0-17.1',
+			'libvirt-libs-debuginfo': '5.1.0-17.1',
+			'libvirt-lock-sanlock': '5.1.0-17.1',
+			'libvirt-lock-sanlock-debuginfo': '5.1.0-17.1',
+			'libvirt-nss': '5.1.0-17.1',
+			'libvirt-nss-debuginfo': '5.1.0-17.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
